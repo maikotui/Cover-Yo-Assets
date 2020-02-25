63a511a7f8cd64fca1688269b85343bfdb45fcad
Maiko Tuitupou Jr <tuitupoumaiko@gmail.com>
2020-02-25 12:51:34 -0700

    [INIT] Initial commit

---
+++ GameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoverYourAssets
+{
+    class GameController
+    {
+        private IGameView view;
+        public GameController(IGameView view)
+        {
+            this.view = view;
+        }
+    }
+}
